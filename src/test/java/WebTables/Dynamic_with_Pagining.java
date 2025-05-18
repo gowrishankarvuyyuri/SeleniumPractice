6a5031c3250a92487bfe35eb5faeb24e22379ac9
gowrishankarvuyyuri <gowrishankarvuyyuri@outlook.com>
2025-05-19 01:33:21 +0530

    Dynamic Pagination

--- src/test/java/WebTables/Dynamic_with_Pagining.java
+++ src/test/java/WebTables/Dynamic_with_Pagining.java
@@ -18,21 +18,58 @@ public class Dynamic_with_Pagining {
 		driver.get("https://testautomationpractice.blogspot.com/");
 		driver.manage().window().maximize();
 		
+		//collecting number of paginations
 		List<WebElement> elements = driver.findElements(By.xpath("//ul[@class='pagination']/li/a"));
 		
 		int count = 1;
+		
+		/* selecting all clicks
+		//iterating them
 		for(WebElement element : elements) {
+			//clicking them individually
 			driver.findElement(By.xpath("//ul[@class='pagination']/li["+count+"]/a")).click();
 			count+=1;
 			
+			//perform actions in individual pagining	
 			List<WebElement> Elements = driver.findElements(By.xpath("//table[@id='productTable']//tbody//input"));
 			int button = 1;
 			
 			for(WebElement Element : Elements) {
-				
+				//selecting them
 				driver.findElement(By.xpath("//table[@id='productTable']//tbody/tr["+button+"]//input")).click();
 				button+=1;
 			}
+		}
+		*/
+		
+		//selecting individual clicks
+		int flag = 0;
+		for(WebElement element : elements) {
+			//clicking them individually
+			driver.findElement(By.xpath("//ul[@class='pagination']/li["+count+"]/a")).click();
+			count+=1;
+			
+			//perform actions in individual pagining	
+			List<WebElement> Elements = driver.findElements(By.xpath("//table[@id='productTable']//tbody//td[2]"));
+			int button = 1;
+			
+			for(WebElement Element : Elements) {
+				//selecting them
+				WebElement indiv = driver.findElement(By.xpath("//table[@id='productTable']//tbody/tr["+button+"]/td[2]"));
+				String Name = indiv.getText();
+				System.out.println(Name);
+				
+				if(Name.equals("Television") || Name.equals("Digital Camera")) {
+					driver.findElement(By.xpath("//table[@id='productTable']//tbody/tr["+button+"]/td[4]/input")).click();
+				}
+				else {
+					button+=1;
+					continue;
+				}
+			}
+			
+			if(flag == 1)
+				break;
 		}
 	}
 }
